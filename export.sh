c4ad176fc4e231d63497f2fb29e4282f2d04931d
Maksym Kupriichuk <30961944+maksymkpr@users.noreply.github.com>
2020-02-23 21:23:52 +0200

    remove passowrd warning from bd.sql

--- export.sh
+++ export.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+_os="`uname`"
 _now=$(date +"%m_%d_%Y_%T")
 _wpdb="wp-db"
 _file="$_wpdb/data_$_now.sql"
 
-mkdir -p $_wpdb
-docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"' > $_file
+docker-compose exec db sh -c 'exec mysqldump "$MYSQL_DATABASE" -hlocalhost  -uroot -p"$MYSQL_ROOT_PASSWORD"' > $_file
+
+
+
+if [[ $_os == "Darwin"* ]] ; then
+  sed -i '.bak' 1,1d $_file
+else
+  sed -i 1,1d $_file # Removes the password warning from the file
+fi
